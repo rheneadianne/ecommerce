356fbe944341881cbf08d25881244f42e64293d9
rheneadianne <rheneadianne@gmail.com>
2022-08-07 09:52:03 -0400

    product columns defined, index edit with foreign key id

--- models/index.js
+++ models/index.js
@@ -6,24 +6,24 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: ''
+  foreignKey: 'id'
 })
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey:''
+  foreignKey:'id'
 })
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey:''
+  foreignKey:'id'
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: ''
+  foreignKey: 'id'
 })
 
 module.exports = {
