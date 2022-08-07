060d958b803c5581f52b9287f5abcf9ba0c434d9
rheneadianne <rheneadianne@gmail.com>
2022-08-07 10:19:01 -0400

    category route done (once again...i forgot to save)

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -19,7 +19,7 @@ router.get('/', (req, res) => {
     }
   })
   .then( data => {
-    data ? res.status(404).json({
+    !data ? res.status(404).json({
       message: 'No categories found! Please try again.'
     })
     : res.json(data)
@@ -33,18 +33,80 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  })
+  .then( data => {
+    !data ? res.status(404).json({
+      message: 'No categories found with that id! Please try again.'
+    })
+    : res.json(data)
+  })
+  .catch( error => {
+    console.error(error);
+    res.status(500).json(error)
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(data => {
+    data ? res.status(404).json({
+      message: 'Category exists! Please try again'
+    })
+    : res.json(data)
+  })
+  .catch( error => {
+    console.error(error);
+    res.status(500).json(error)
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then( data => {
+    !data ? res.status(404).json({
+      message: 'No categories found with that id! Please try again.'
+    })
+    : res.json(data)
+  })
+  .catch( error => {
+    console.error(error);
+    res.status(500).json(error)
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then( data => {
+    !data ? res.status(404).json({
+      message: 'No categories found with that id! Please try again.'
+    })
+    : res.json(data)
+  })
+  .catch( error => {
+    console.error(error);
+    res.status(500).json(error)
+  })
 });
 
 module.exports = router;
