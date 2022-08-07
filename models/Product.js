f15b4f3c3bfa3b4dcfde80f7b2bf4ba052fcc256
rheneadianne <rheneadianne@gmail.com>
2022-08-07 09:52:47 -0400

    product columns defined, index edit with foreign key id (saved this time T.T)

--- models/Product.js
+++ models/Product.js
@@ -11,6 +11,35 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    // id primary key
+    // product_name: 'Plain T-Shirt',
+    // price: 14.99, //validate if decimal
+    // stock: 14,
+    // category_id: 1,
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type:DataTypes.DECIMAL,
+      allowNull:false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
